a892585ed6f506a3fd0860f1d2ecbda931b16ed5
Jaitry Patel <jaitry.patel@1Rintern.local>
2023-05-30 17:30:12 +0530

    files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+// import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // standalone: true,
 })
 export class AppComponent {
   title = 'binding';
+
+  name : string = 'Jaitry';
+
+  
+
+/**
+//  * @title Overlay basic example
+ */
+
+  
+// export class CdkOverlayBasicExample {
+  isOpen = false;
+// }
+  
 }
